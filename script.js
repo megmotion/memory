5553e8d4a51fec53b872b632e404c06d2bf7a6c2
megmotion <33497517+megmotion@users.noreply.github.com>
2019-03-14 02:02:44 +0100

    timer and moves counter

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const cards = document.querySelectorAll('.memory-card');
-
+let overlays = Array.from(document.getElementsByClassName('overlay-text'));
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let timer = document.getElementById('time');
+let ticker = document.getElementById('flips');
+let matchedCards = [];
+let totalClicks = 0;
+let totalTime = 0;
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+overlays.forEach(overlay => {
+	overlay.addEventListener('click', () => {
+		overlay.classList.remove('visible');
+		startNewGame();
+		});
+});
 
 function flipCard () {
 	if (lockBoard) return;
-	if (this === firstCard) return
+	if (this === firstCard) return;
 	this.classList.add('flip');
+	totalClicks++;
+	ticker.innerText = totalClicks;
+	console.log(totalClicks);
 
 	if (!hasFlippedCard) {
 		hasFlippedCard = true;
@@ -26,14 +42,24 @@ function flipCard () {
 function checkForMatch() {
 	let isMatch = firstCard.dataset.framework === secondCard.dataset.framework 
 		
-	isMatch ? disableCards() : unflipCards()
+	isMatch ? hideCards() : unflipCards()
 }
 
-function disableCards() {
+function hideCards() {
 	firstCard.removeEventListener('click',flipCard);
 	secondCard.removeEventListener('click',flipCard); 
 
-	resetBoard();
+	matchedCards++;
+    matchedCards++;
+    lockBoard = true;
+	setTimeout(() => {
+		firstCard.classList.add('hide');
+		secondCard.classList.add('hide');
+
+		resetBoard();
+	}, 500);
+
+	if (matchedCards === 32) victory();
 }
 
 function unflipCards() {
@@ -43,7 +69,7 @@ function unflipCards() {
 		secondCard.classList.remove('flip');
 
 		resetBoard();
-	}, 1300);
+	}, 1000);
 }
 
 function resetBoard() {
@@ -51,11 +77,28 @@ function resetBoard() {
 	[firstCard, secondCard] = [null, null];
 }
 
-(function shuffle () {
+function victory() {
+	document.getElementById('victory-text').classList.add('visible');
+}
+function startTimer() {
+	setInterval (function() {
+	timer.innerHTML = totalTime++;
+	}, 1000);
+}
+
+function startNewGame() {
+	matchedCards = [];
+	totalClicks = 0;
+	ticker.innerText = totalClicks;
+	totalTime = 0;
+	cards.forEach(card => card.classList.remove('hide'));
+	cards.forEach(card => card.classList.remove('flip'));
+	cards.forEach(card => card.addEventListener('click', flipCard));
+	startTimer();
+}
+/*(function shuffle() {
 	cards.forEach(card => {
 		let randomPos = Math.floor(Math.random()*32);
 		card.style.order = randomPos;
 	})
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
+})();*/
